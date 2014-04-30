6aaa501ca36471e956a5e5fbe465b0f3e8312f67
Jeff Walker <walker.jeff.d@gmail.com>
2014-04-30 13:44:51 -0400

    update requirements, add settings templates

---
+++ mysite/mysite/settings/production_template.py
@@ -0,0 +1,45 @@
+from .base import *
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': '<db engine>',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '<db name>',        # Or path to database file if using sqlite3.
+        'USER': '<db username>',
+        'PASSWORD': '<db password>',
+        'HOST': '<db host>',        # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                            # Set to empty string for default.
+    }
+}
+
+# Hosts/domain names that are valid for this site; required if DEBUG is False
+# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
+ALLOWED_HOSTS += ['<domain name>']
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/var/www/example.com/media/"
+MEDIA_ROOT = '<abs path to server media>'
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://example.com/media/", "http://media.example.com/"
+MEDIA_URL = '/media/'
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/var/www/example.com/static/"
+STATIC_ROOT = '<abs path to static media>'
+
+# URL prefix for static files.
+# Example: "http://example.com/static/", "http://static.example.com/"
+STATIC_URL = '<url to static media>'
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    '<abs path to server templates>',
+)
