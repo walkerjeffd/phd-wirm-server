6922f5bb752203a912de2186038329c4664ab00f
Jeff Walker <walker.jeff.d@gmail.com>
2014-04-30 13:58:16 -0400

    removed files

--- passenger_wsgi_test.py
+++
@@ -1,9 +0,0 @@
-import sys, os, django
-
-sys.path.append("/home/walkerjeffd/wirm.walkerjeff.com/mysite/")
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings.production'
-
-def application(environ, start_response):
-    write = start_response('200 OK', [('Content-type', 'text/plain')])
-    return ["Hello, world!"]
