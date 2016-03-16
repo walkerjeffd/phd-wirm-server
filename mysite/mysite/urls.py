91589a5d1578861ffb8379a43bf2a066f199d194
walkerjeffd <walker.jeff.d@gmail.com>
2016-03-15 21:15:29 -0400

    update for gunicorn, replace walkerjeff.com with walkerenvres.com

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -15,3 +16,5 @@ urlpatterns = patterns('mysite.views',
     url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
